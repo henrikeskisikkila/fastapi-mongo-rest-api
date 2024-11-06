608aae08d93e4ba3a5cc086352f14010a707cc90
Henri <henri@keskisikkila.fi>
2024-11-06 20:38:50 +0200

    initial server startup and shutdown

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-print(config["VARIABLE"])
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up")
+    yield
+    print("Shutting down")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World!"}
