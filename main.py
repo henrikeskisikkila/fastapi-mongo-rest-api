f7f4cb5fef82d844ebf06a3a6915464aa5ae0215
Henri <henri@keskisikkila.fi>
2024-11-08 20:46:07 +0200

    add mongodb connection

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from dotenv import dotenv_values
+from pymongo import MongoClient
 from routes import router
 
 config = dotenv_values(".env")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print("Starting up")
+    app.mongodb_client = MongoClient(config["MONGO_URL"])
+    app.database = app.mongodb_client[config["MONGO_DB"]]
     yield
-    print("Shutting down")
+    app.mongodb_client.close()
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(router, prefix=config["API_PREFIX"])
+app.include_router(router, tags=["todo"], prefix=config["API_PREFIX"])
