f9e58ac69f264ac674eee675e4165e97f3026253
Henri <henri@keskisikkila.fi>
2024-11-08 21:16:42 +0200

    add get todos

--- routes.py
+++ routes.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Body
+from fastapi.encoders import jsonable_encoder
+from models import TodoItem
 
 router = APIRouter()
 
-@router.get("/ping", response_description="Ping pong", status_code=200)
-async def ping():
-    return {"ping": "pong"}
+@router.post("/todos", response_description="Create a new todo", response_model=TodoItem, status_code=201)
+async def create_todo(request: Request, todo: TodoItem = Body(...)):
+    todo = jsonable_encoder(todo)
+    new_todo = request.app.database["todos"].insert_one(todo)
+    created_todo = request.app.database["todos"].find_one({"_id": new_todo.inserted_id})
+    return created_todo
+
+@router.get("/todos", response_description="List all todos", response_model=list[TodoItem])
+async def list_todos(request: Request):
+    todos = request.app.database["todos"].find(limit=100)
+    return [todo for todo in todos]
